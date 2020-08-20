2b137c492fa8067aee55581f4ec2f883c36d599d
JohnyKovalenko1337 <13371488zeus@gmail.com>
2020-08-20 22:03:58 +0300

    added model for tasks

---
+++ server/database/models/tasks.js
@@ -0,0 +1,45 @@
+const fs = require('fs');
+const path = require('path');
+
+const p = path.join(
+  path.dirname(process.mainModule.filename),
+  'storage',
+  'tasks.json'
+);
+
+const getTaskFromFile = cb => {
+  fs.readFile(p, (err, fileContent) => {
+    if (err) {
+      cb([]);
+    } else {
+      cb(JSON.parse(fileContent));
+    }
+  });
+};
+
+module.exports = class Task {
+  constructor(title, description, userId) {
+    this.title = title;
+    this.description = description;
+    this.userId = userId;
+  }
+
+  save() {
+    getTaskFromFile(tasks => {
+      tasks.push(this);
+      fs.writeFile(p, JSON.stringify(tasks), err => {
+        console.log(err);
+      });
+    });
+  }
+
+  static fetchAll(cb) {
+    getTaskFromFile(cb);
+  }
+
+  static deleteById(id) {
+    getTaskFromFile(tasks => {
+      tasks.splice(id, 1);
+    });
+  }
+};
