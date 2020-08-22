d94dce016f86e1b6d9a2f05bea0b80ea4e8cafcc
JohnyKovalenko1337 <13371488zeus@gmail.com>
2020-08-22 17:51:33 +0300

    added signup controller

--- server/controllers/user-controller.js
+++ server/controllers/user-controller.js
@@ -1,16 +1,19 @@
 const User = require('../models/users');
 
-exports.userGet = async (req,res,next) =>{
+exports.userGet = async (req, res, next) => {
     const username = req.body.username;
 
     const user = await User.findByName(username);
 
-    if(!user){
-        return res.json({message:"no user with this username"});
+    if (!user) {
+        return res.json({ message: "no user with this username" });
     }
-    return res.json({user: user});
+    return res.json({ user: user });
 };
 
-exports.signup = (req,res,next) =>{
-
-};
+exports.signup = (req, res, next) => {
+    const { username, password } = req.body;
+    const user = new User(username, password)
+    user.save()
+    return res.status(200).json({ message: "success" });
+}        
