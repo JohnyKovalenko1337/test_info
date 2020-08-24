bb133086f56585fc1d608e0888f58118b54d3137
JohnyKovalenko1337 <13371488zeus@gmail.com>
2020-08-24 11:43:50 +0300

    changed rout

--- server/routes/user-routes.js
+++ server/routes/user-routes.js
@@ -5,7 +5,7 @@ const userController = require('../controllers/user-controller');
 const Router = express.Router();
 
 Router.post('/signup', userController.signup)
-Router.post('/login',  userController.userGet);
+Router.post('/getUser',  userController.userGet);
 
 
 module.exports = Router;
