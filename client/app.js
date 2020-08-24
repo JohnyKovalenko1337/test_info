837bff81ad8677276edee233a9aa9cf6f2036b9b
JohnyKovalenko1337 <13371488zeus@gmail.com>
2020-08-24 11:41:51 +0300

    added cli signup

--- client/app.js
+++ client/app.js
@@ -1,14 +1,14 @@
 'use strict';
 
 const readline = require('readline');
-
+const { loginCheck, signup } = require('./requests/auth');
 const rl = readline.createInterface({
   input: process.stdin,
   output: process.stdout,
   prompt: '> ',
+
 });
 
-let logging = false;
 
 console.log('                | Welcome to ToDo app |');
 const loggined = false;
@@ -23,13 +23,9 @@ if (!loggined) {
       console.log('Available commands: ', Object.keys(commands).join(', '));
     },
     login() {
-      let login;
-      console.clear();
-      logging = true;
-      rl.question('Enter login', (line)=>{
-        login = line;
-        console.log(`ur login is ${login}`);
-      })
+
+
+
       /* console.log('Enter login');
       rl.prompt()
       rl.on('line', line => {
@@ -38,7 +34,33 @@ if (!loggined) {
       }); */
     },
     signup() {
+      let login;
+      let password;
+      console.clear();
+      rl.stdoutMuted = true;
 
+      rl.question(
+        `          ┌────────────────────────────┐
+          │ Enter login                │
+          └────────────────────────────┘\n        > `,
+        (line) => {
+          login = line;
+          if (!loginCheck(login)) {
+            rl.question(
+              `          ┌────────────────────────────┐
+          │ Enter password             │
+          └────────────────────────────┘\n        > `,
+              (pass) => {
+                password = pass;
+                signup(login, password);
+                console.log(" Authorized !")
+              }
+            );
+            rl.prompt();
+
+          }
+
+        })
     }
 
   };
@@ -64,7 +86,7 @@ rl.on('line', line => {
   if (line === 'login') {
     const command = commands[line];
     command();
-    rl.on('pause', ()=>{});
+    rl.on('pause', () => { });
   }
   else {
     const command = commands[line];
