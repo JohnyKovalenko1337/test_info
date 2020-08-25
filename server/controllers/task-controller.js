602eef9a8bd585a92ebc38a856573b5a9bca9eac
JohnyKovalenko1337 <13371488zeus@gmail.com>
2020-08-25 21:26:11 +0300

    added create and get tasks

--- server/controllers/task-controller.js
+++ server/controllers/task-controller.js
@@ -1,7 +1,31 @@
 const Task = require('../models/tasks');
+const User = require('../models/users');
 
 exports.getAllTasks = (req,res,next) => {
     Task.fetchAll((tasks)=>{
         return res.status(200).json(tasks);
     });
+};
+
+exports.createTask = (req,res,next) => {
+    const {title, description, creator} = req.body;
+    const task = new Task (title, description, creator);
+    task.save();
+    return res.status(200).json({ message: "success" });
+};
+
+exports.getMyTasks = (req,res,next) => {
+    const creator = req.body.creator;
+    Task.findByCreator(creator, (tasks)=>{
+        console.log(tasks);
+        return res.json({tasks: tasks});
+    })
+}
+
+exports.updateById = (req,res,next) =>{
+
+};
+
+exports.deleteById = (req,res,next) =>{
+
 };
